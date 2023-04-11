bc01f6fcd0a4999814b07ae3b1ed38c22c7efed0
Heron Wake <georgerincon2@gmail.com>
2023-04-11 02:18:30 -0400

    tweaking build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './app/public/client/views/index.html',
+  entry: './app/public/server/server.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
