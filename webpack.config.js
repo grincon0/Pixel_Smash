daf0463dd7c61352827b67023ec57adec2062d17
Heron Wake <georgerincon2@gmail.com>
2023-04-11 01:48:57 -0400

    testing webpack polyfill workaround

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,32 @@ module.exports = {
   entry: './app/public/server/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+  },
+  resolve: {
+    fallback: {
+      "fs": false,
+      "net": false,
+      "tls": false,
+      "child_process": false,
+      "dns": false,
+      "crypto": false,
+      "os": false,
+      "path": false,
+      "http2": false,
+      "http": false,
+      "https": false,
+      "stream": false,
+      "zlib": false,
+      "querystring": false,
+      "url": false,
+/*       "zlib": require.resolve("browserify-zlib"),
+      "querystring": require.resolve("querystring-es3"),
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "buffer": require.resolve("buffer"),
+      "util": require.resolve("util"),
+      "assert": require.resolve("assert") */
+    }
   }
 };
